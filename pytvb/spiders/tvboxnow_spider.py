76451bbd5426034181e5b97b8152e888984008a8
Jason Ma <jasonsma@users.noreply.github.com>
2015-02-11 16:44:02 -0600

    Adding regex matches for most whyhung and one jacky for the forum8-1 page
      - matches title, first and last episode

--- pytvb/spiders/tvboxnow_spider.py
+++ pytvb/spiders/tvboxnow_spider.py
@@ -1,8 +1,11 @@
+# -*- coding: utf-8 -*-
 from scrapy.spider import BaseSpider
 from scrapy.http import FormRequest, Request
 from scrapy.selector import Selector
 
 from pytvb.items import Scrapy81Item, ScrapyThreadItem
+
+import re
 
 URL_BASE = 'http://www.tvboxnow.com/'
 
@@ -41,21 +44,69 @@ class LoginSpider(BaseSpider):
             author = entry.xpath("td[@class='author']/cite/a/text()").extract()
             date = entry.xpath("td[@class='author']/em/text()").extract()
             link = entry.xpath("th[@class='subject hot']/span[@id]/a/@href").extract()
-            if title and author and date:
-                title[0].encode('utf-8')
-                print title[0], author[0], date[0], link[0]
+            if title_row and author and date:
+                title_row[0].encode('utf-8')
+                #print title_row[0], author[0], date[0], link[0]
+                print self.extract_title_row(title_row[0])
                 threads.append(Request(url = URL_BASE + link[0], callback = self.parseThreads))
 
         return threads[0]
 
+    def extract_title_row(self, row):
+        # match:
+        # [更新第2集] (高清翡翠台) 《倩女喜相逢》- 第1~2集 [HDTV-DIVX+HDTV-RMVB480P+HDTV-RMVB720P][粵語中字]
+        whyhung1 = re.compile(r'\[更新第(\d+)集.*?\]\s*\(.+?\)\s*《(.+?)》\s*-\s*第(\d+)~(\d+)集')
+
+        # [更新第30集(完)] (TVB) 大太監 第1~31集(完) [TV-RMVB+AVI+HDTV-RMVB720P][粵語中字]
+        # [更新第30+31集(完)] (TVB) 大太監 第1~31集(完) [TV-RMVB+AVI+HDTV-RMVB720P][粵語中字]
+        # group(2) would be None for the first case
+        whyhung2 = re.compile(r'\[更新第(\d+)\+?(\d+)?集.*?\]\s*\(.+?\)\s*(.+?)\s+第(\d+)~(\d+)集')
+
+        # [更新Ch41(完)] (煲劇2台)《宮鎖連城》 Ch01~44(完) [2015-02-02][TV-RMVB][粵語配音中字][720x408]
+        # [更新Ch41~44(完)] (煲劇2台)《宮鎖連城》 Ch01~44(完) [2015-02-02][TV-RMVB][粵語配音中字][720x408]
+        # [更新Ch700] (高清翡翠台) 愛．回家 Ch600~700 [高清->RMVB 版本][粵語中字][720x408]
+        whyhung3 = re.compile(r'\[更新Ch(\d+)~?(\d+)?.*?\]\s*\(.+?\)\s*(?:《)?(.+?)(?:》)?\s*Ch(\d+)~(\d+)')
+
+        # [更新Ch02](TVB) 倩女喜相逢 [Ch01 - Ch02] [TV-RMVB][粵語中字]
+        jacky1 = re.compile(r'\[更新Ch(\d+)~?(\d+)?.*?\]\s*\(.+?\)\s*(?:《)?(.+?)(?:》)?\s*\[?Ch(\d+)\s*-\s*Ch(\d+)')
+
+        row = row.encode('utf8')
+        m = whyhung1.search(row)
+        if m:
+            title = m.group(2)
+            first_episode = int(m.group(3))
+            last_episode = int(m.group(4))
+            return title, first_episode, last_episode
+
+        m = whyhung2.search(row)
+        if m:
+            title = m.group(3)
+            first_episode = int(m.group(4))
+            last_episode = int(m.group(5))
+            return title, first_episode, last_episode
+
+        m = whyhung3.search(row)
+        if m:
+            title = m.group(3)
+            first_episode = int(m.group(4))
+            last_episode = int(m.group(5))
+            return title, first_episode, last_episode
+
+        m = jacky1.search(row)
+        if m:
+            title = m.group(3)
+            first_episode = int(m.group(4))
+            last_episode = int(m.group(5))
+            return title, first_episode, last_episode
+
     def parseThreads(self, response):
         open('forumthread', 'wb').write(response.body)
         links = response.xpath("//span[@id]/a")
-        print "parsing %s\n" % response.url
+        #print "parsing %s\n" % response.url
         for link in links:
             title = link.xpath('strong/text()').extract()
             href = link.xpath('@href').extract()
             if title and href:
                 title[0].encode('utf-8')
-                print title[0], href[0]
+                #print title[0], href[0]
         return None
